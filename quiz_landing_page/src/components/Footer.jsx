12a8e4e0124e6e2fca1c79f99170e2d8160135ff
iammmila <ibramila2002@gmail.com>
2023-01-09 00:22:23 +0400

    footer_bottom

--- quiz_landing_page/src/components/Footer.jsx
+++ quiz_landing_page/src/components/Footer.jsx
@@ -9,33 +9,44 @@ function Footer() {
     <footer>
       <div className="container">
         <div className="footer">
-          <ul>
-            <h5>Menu</h5>
-            <li>About</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>Contact</li>
-          </ul>
-          <ul>
-            <h5>Service</h5>
-            <li>planning</li>
-            <li>management</li>
-            <li>strategy</li>
-            <li>Market</li>
-          </ul>
-          <ul>
-            <li>
-              <img src={facebook} alt="facebook" />
-            </li>
-            <li>
-              <img src={twitter} alt="twitter" />
-            </li>
-            <li>
-              <img src={instagram} alt="instagram " />
-            </li>
-          </ul>
+          <div className="footer_top">
+            <ul>
+              <h5>Menu</h5>
+              <li>About</li>
+              <li>Services</li>
+              <li>Blog</li>
+              <li>Contact</li>
+            </ul>
+            <ul>
+              <h5>Service</h5>
+              <li>planning</li>
+              <li>management</li>
+              <li>strategy</li>
+              <li>Market</li>
+            </ul>
+            <ul className='medias'>
+              <li>
+                <img src={facebook} alt="facebook" />
+              </li>
+              <li>
+                <img src={twitter} alt="twitter" />
+              </li>
+              <li>
+                <img src={instagram} alt="instagram " />
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <div className="footer_bottom">
+            <span>Copyright © 2022 Quizbaj. All Rights Reserved.</span>
+            <div className='right_side'>
+              <span>Privacy Policy</span>
+              <span>Terms of Use</span>
+            </div>
+          </div>
         </div>
       </div>
+
     </footer>
   )
 }
