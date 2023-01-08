5f435de2af1569361b0d87346da91c435d4f728e
iammmila <ibramila2002@gmail.com>
2023-01-08 22:44:53 +0400

    Section6

---
+++ quiz_landing_page/src/components/Section6.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./../assets/scss/_section6.scss";
+
+function Section6() {
+  return (
+    <section>
+        
+    </section>
+  )
+}
+
+export default Section6
--- quiz_landing_page/src/pages/Home.jsx
+++ quiz_landing_page/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Section2 from '../components/Section2';
 import Section3 from '../components/Section3';
 import Section4 from '../components/Section4';
 import Section5 from '../components/Section5';
+import Section6 from '../components/Section6';
 
 function Home() {
     return (
@@ -17,6 +18,7 @@ function Home() {
             <Section3 />
             <Section4 />
             <Section5 />
+            <Section6 />
         </HelmetProvider>
     )
 }
